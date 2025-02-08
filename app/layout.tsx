12b2c8a79446518695ee840f8a6c35089157ca07
Kedar <66516089+kedarawale@users.noreply.github.com>
2025-02-08 01:13:13 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+        <div id="hidden-clustrmap" style={{ display: 'none' }}>
+          <a href="https://clustrmaps.com/site/1c497"  title="ClustrMaps"><img src="//www.clustrmaps.com/map_v2.png?d=47DeUMbqLl8unnmGyeUv9ZLAOvrFHBHJwELbh81ZTbY&cl=ffffff" /></a>
+        </div>
+      </body>
     </html>
   )
 }
